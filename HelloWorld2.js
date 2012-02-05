b7dddf511312bb8de6bc8d12bb4f71dad07ac5b1
ian7682 <ianacrow@hotmail.com>
2012-02-05 22:57:25 +0000

    first nodejs example

--- HelloWorld2.js
+++ HelloWorld2.js
@@ -1,1 +1,5 @@
-#My new file
+var http = require('http');
+http.createServer(function (req, res) {
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.end('Hello World\n');
+}).listen(process.env.PORT);
